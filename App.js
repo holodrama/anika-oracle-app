d294f80d73f8a75fd5eaa4cabb25deab9bc06411
holodrama <holodrama2@yahoo.com>
2025-08-22 12:35:12 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Anika Oracle App</h1>
+      <p>This is a clean and fresh version.</p>
+    </div>
+  );
+}
+
+export default App;
